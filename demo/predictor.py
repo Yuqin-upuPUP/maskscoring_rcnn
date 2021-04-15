8ad66ed4c9831f848a2900e52cf14909cd21e4d6
liyuqin <617457831@qq.com>
2021-04-16 01:37:10 +0800

    modify the predictor.py

--- demo/predictor.py
+++ demo/predictor.py
@@ -1,9 +1,7 @@
 # Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
-import cv2
-import random
-import colorsys
-import torch
 import numpy as np
+import cv2
+import torch
 from torchvision import transforms as T
 from torchvision.transforms import functional as F
 from maskrcnn_benchmark.modeling.detector import build_detection_model
@@ -46,31 +44,6 @@ class Resize(object):
         size = self.get_size(image.size)
         image = F.resize(image, size)
         return image
-
-
-def apply_mask(image, mask, color, alpha=0.5):
-    """
-        Apply the given mask to the image.
-        """
-    for c in range(3):
-        image[:, :, c] = np.where(mask == 1,
-                                  image[:, :, c] * (1 - alpha) + alpha * color[c],
-                                  image[:, :, c])
-    return image
-
-
-def random_colors(N, bright=True):
-    """
-    Simple function that adds fixed colors depending on the class
-    Arguments:
-        N: the number of object
-    """
-    brightness = 1.0 if bright else 0.7
-    hsv = [(i / N, 1, brightness) for i in range(N)]
-    colors = list(map(lambda c: colorsys.hsv_to_rgb(*c), hsv))
-    colors = [tuple(color * 255 for color in i) for i in colors]
-    random.shuffle(colors)
-    return colors
 
 
 class COCODemo(object):
@@ -263,26 +236,18 @@ class COCODemo(object):
                 the BoxList via `prediction.fields()`
         """
         predictions = self.compute_prediction(image)
-        top_predictions = self.select_top_predictions(predictions)      # 经过置信度过滤的mask
+        top_predictions = self.select_top_predictions(predictions)
 
         result = image.copy()
-        labels = top_predictions.get_field("labels")
-        material_labels = top_predictions.get_field("material_labels")
-        boxes = top_predictions.bbox
-        masks = top_predictions.get_field("mask").numpy()
-        scores = top_predictions.get_field("scores")
-        material_scores = top_predictions.get_field("material_scores")
-        colors = random_colors(labels.shape[0])
-
+        if self.show_mask_heatmaps:
+            return self.create_mask_montage(result, top_predictions)
+        result = self.overlay_boxes(result, top_predictions)
         if self.cfg.MODEL.MASK_ON:
-            result = self.overlay_mask(result, colors, masks)
-        result = self.overlay_boxes(result, colors, boxes)
-        result = self.overlay_class_names(result, scores, labels, material_scores, material_labels, boxes)
-
-        # if self.show_mask_heatmaps:
-        #     return self.create_mask_montage(result, top_predictions)
+            result = self.overlay_mask(result, top_predictions)
         # if self.cfg.MODEL.KEYPOINT_ON:
         #     result = self.overlay_keypoints(result, top_predictions)
+        result = self.overlay_class_names(result, top_predictions)
+
         return result
 
     def compute_prediction(self, original_image):
@@ -340,7 +305,15 @@ class COCODemo(object):
         _, idx = scores.sort(0, descending=True)
         return predictions[idx]
 
-    def overlay_boxes(self, image, colors, boxes):
+    def compute_colors_for_labels(self, labels):
+        """
+        Simple function that adds fixed colors depending on the class
+        """
+        colors = labels[:, None] * self.palette
+        colors = (colors % 255).numpy().astype("uint8")
+        return colors
+
+    def overlay_boxes(self, image, predictions):
         """
         Adds the predicted boxes on top of the image
         Arguments:
@@ -348,6 +321,11 @@ class COCODemo(object):
             predictions (BoxList): the result of the computation by the model.
                 It should contain the field `labels`.
         """
+        labels = predictions.get_field("labels")
+        boxes = predictions.bbox
+
+        colors = self.compute_colors_for_labels(labels).tolist()
+
         for box, color in zip(boxes, colors):
             box = box.to(torch.int64)
             top_left, bottom_right = box[:2].tolist(), box[2:].tolist()
@@ -357,7 +335,7 @@ class COCODemo(object):
 
         return image
 
-    def overlay_mask(self, image, colors, masks):
+    def overlay_mask(self, image, predictions):
         """
         Adds the instances contours for each predicted object.
         Each label has a different color.
@@ -366,16 +344,30 @@ class COCODemo(object):
             predictions (BoxList): the result of the computation by the model.
                 It should contain the field `mask` and `labels`.
         """
+        masks = predictions.get_field("mask").numpy()
+        labels = predictions.get_field("labels")
+
+        colors = self.compute_colors_for_labels(labels).tolist()
+
         for mask, color in zip(masks, colors):
             thresh = mask[0, :, :, None].astype(np.uint8)
             contours, hierarchy = cv2_util.findContours(
                 thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE
             )
-            image = cv2.drawContours(image, contours, -1, color)
-            image = apply_mask(image, mask, color)
+            image = cv2.drawContours(image, contours, -1, color, 3)
+
         composite = image
 
         return composite
+
+    # def overlay_keypoints(self, image, predictions):
+    #     keypoints = predictions.get_field("keypoints")
+    #     kps = keypoints.keypoints
+    #     scores = keypoints.get_field("logits")
+    #     kps = torch.cat((kps[:, :, 0:2], scores[:, :, None]), dim=2).numpy()
+    #     for region in kps:
+    #         image = vis_keypoints(image, region.transpose((1, 0)))
+    #     return image
 
     def create_mask_montage(self, image, predictions):
         """
@@ -412,7 +404,7 @@ class COCODemo(object):
                 result[start_y:end_y, start_x:end_x] = masks[y, x]
         return cv2.applyColorMap(result.numpy(), cv2.COLORMAP_JET)
 
-    def overlay_class_names(self, image, scores, labels, material_scores, material_labels, boxes):
+    def overlay_class_names(self, image, predictions):
         """
         Adds detected class names and scores in the positions defined by the
         top-left corner of the predicted bounding box
@@ -421,15 +413,86 @@ class COCODemo(object):
             predictions (BoxList): the result of the computation by the model.
                 It should contain the field `scores` and `labels`.
         """
+        scores = predictions.get_field("scores").tolist()
+        labels = predictions.get_field("labels").tolist()
+        material_scores = predictions.get_field("material_scores").tolist()
+        material_labels = predictions.get_field("material_labels").tolist()
         labels = [self.CATEGORIES[i] for i in labels]
         material_labels = [self.MATERIALS[i] for i in material_labels]
+        boxes = predictions.bbox
 
         template = "{}: {:.2f} {}: {:.2f}"
         for box, score, label, material_score, material_label in zip(boxes, scores, labels, material_scores, material_labels):
             x, y = box[:2]
             s = template.format(label, score, material_label, material_score)
             cv2.putText(
-                image, s, (x, y + 10), cv2.FONT_HERSHEY_SIMPLEX, .5, (255.0, 255.0, 255.0), 1
+                image, s, (x, y), cv2.FONT_HERSHEY_SIMPLEX, .5, (255, 255, 255), 1
             )
 
         return image
+
+#
+# import numpy as np
+# import matplotlib.pyplot as plt
+# from maskrcnn_benchmark.structures.keypoint import PersonKeypoints
+#
+#
+# def vis_keypoints(img, kps, kp_thresh=2, alpha=0.7):
+#     """Visualizes keypoints (adapted from vis_one_image).
+#     kps has shape (4, #keypoints) where 4 rows are (x, y, logit, prob).
+#     """
+#     dataset_keypoints = PersonKeypoints.NAMES
+#     kp_lines = PersonKeypoints.CONNECTIONS
+#
+#     # Convert from plt 0-1 RGBA colors to 0-255 BGR colors for opencv.
+#     cmap = plt.get_cmap('rainbow')
+#     colors = [cmap(i) for i in np.linspace(0, 1, len(kp_lines) + 2)]
+#     colors = [(c[2] * 255, c[1] * 255, c[0] * 255) for c in colors]
+#
+#     # Perform the drawing on a copy of the image, to allow for blending.
+#     kp_mask = np.copy(img)
+#
+#     # Draw mid shoulder / mid hip first for better visualization.
+#     mid_shoulder = (
+#                            kps[:2, dataset_keypoints.index('right_shoulder')] +
+#                            kps[:2, dataset_keypoints.index('left_shoulder')]) / 2.0
+#     sc_mid_shoulder = np.minimum(
+#         kps[2, dataset_keypoints.index('right_shoulder')],
+#         kps[2, dataset_keypoints.index('left_shoulder')])
+#     mid_hip = (
+#                       kps[:2, dataset_keypoints.index('right_hip')] +
+#                       kps[:2, dataset_keypoints.index('left_hip')]) / 2.0
+#     sc_mid_hip = np.minimum(
+#         kps[2, dataset_keypoints.index('right_hip')],
+#         kps[2, dataset_keypoints.index('left_hip')])
+#     nose_idx = dataset_keypoints.index('nose')
+#     if sc_mid_shoulder > kp_thresh and kps[2, nose_idx] > kp_thresh:
+#         cv2.line(
+#             kp_mask, tuple(mid_shoulder), tuple(kps[:2, nose_idx]),
+#             color=colors[len(kp_lines)], thickness=2, lineType=cv2.LINE_AA)
+#     if sc_mid_shoulder > kp_thresh and sc_mid_hip > kp_thresh:
+#         cv2.line(
+#             kp_mask, tuple(mid_shoulder), tuple(mid_hip),
+#             color=colors[len(kp_lines) + 1], thickness=2, lineType=cv2.LINE_AA)
+#
+#     # Draw the keypoints.
+#     for l in range(len(kp_lines)):
+#         i1 = kp_lines[l][0]
+#         i2 = kp_lines[l][1]
+#         p1 = kps[0, i1], kps[1, i1]
+#         p2 = kps[0, i2], kps[1, i2]
+#         if kps[2, i1] > kp_thresh and kps[2, i2] > kp_thresh:
+#             cv2.line(
+#                 kp_mask, p1, p2,
+#                 color=colors[l], thickness=2, lineType=cv2.LINE_AA)
+#         if kps[2, i1] > kp_thresh:
+#             cv2.circle(
+#                 kp_mask, p1,
+#                 radius=3, color=colors[l], thickness=-1, lineType=cv2.LINE_AA)
+#         if kps[2, i2] > kp_thresh:
+#             cv2.circle(
+#                 kp_mask, p2,
+#                 radius=3, color=colors[l], thickness=-1, lineType=cv2.LINE_AA)
+#
+#     # Blend the keypoints.
+#     return cv2.addWeighted(img, 1.0 - alpha, kp_mask, alpha, 0)
